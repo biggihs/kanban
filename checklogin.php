0bd8dd3fa3780bc27f7548f1642e8a8fd4f07ec5
Birgir Hrafn Sigurðsson <birgir@T420.(none)>
2013-07-29 14:39:13 +0000

    debugging

--- checklogin.php
+++ checklogin.php
@@ -14,9 +14,9 @@ $hashpass = md5($pass);
 //$result = $db->query("SELECT COUNT(*) AS count,api_key FROM users WHERE username = '$user' AND password = '$hashpass'");
 
 $db = pg_connection();
-$query = "SELECT COUNT(*) AS count,api_key FROM users WHERE username = '$user' AND password = '$hashpass' GROUP_BY api_key";
-echo "THE QUERY";
-echo $query;
+$query = "SELECT COUNT(*) AS count, api_key FROM users WHERE username = '$user' AND password = '$hashpass' GROUP_BY api_key";
+echo "THE QUERY\n";
+echo $query."\n";
 $result = pg_query($db,$query);
 
 $rows = $result->fetchAll();
