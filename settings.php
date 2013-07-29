f8926f12c280063a593f640584469c59c17ce135
Birgir Hrafn Sigurðsson <birgir@T420.(none)>
2013-07-29 14:58:30 +0000

    Settings

---
+++ settings.php
@@ -0,0 +1,9 @@
+<?php
+require_once('isloggedin.php');
+
+$codebaseAccount = 'transmit';
+$codebaseUser    = 'birgir';
+$codebaseApikey  = $_SESSION['apikey'];
+$codebaseMainProject = 'london-cru';
+
+$ciUrl = 'http://localhost';
